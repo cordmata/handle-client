a524aba2f4a79ac3a04bba528b400f84b144f7fe
mcordial@ASU.EDU <mcordial@ASU.EDU@4eb6a20f-1087-4917-96d8-8f6e343b9707>
2010-09-17 19:11:45 +0000

    Enabled autogeneration of handles. Added doc.

--- handle/client.py
+++ handle/client.py
@@ -1,57 +1,113 @@
 from httplib2 import Http 
 from urllib import urlencode, quote
 
-NO_CACHE = {'cache-control':'no-cache'} 
+NO_CACHE = ('cache-control', 'no-cache') 
+FORM_ENCODED = ('Content-type', 'application/x-www-form-urlencoded')
 
 class Client(object):
+    '''This is a client for interacting with the ASU Handle 
+    Administration Web Service.'''
+    
+    REGISTRY_BASE_URL = 'http://hdl.handle.net/'
     
     def __init__(self, url, username, password):
         self.baseurl = url if url.endswith('/') else url + '/'
         self.http = Http()
         self.http.add_credentials(username, password)
         
-    def create(self, handle, target):
-        resp, cont = self.http.request(self._request_url(handle, target=target),
-                                       'POST', headers=NO_CACHE)
+    def create(self, target, prefix, suffix=None):
+        '''Request a handle be created for the supplied target under the 
+        supplied prefix. If no suffix is supplied one will be generated.
+        
+        @param target: String representing a valid URL, malformed URLs will
+            result in a BadRequest exception.
+            
+        @param prefix: Prefix of the handle that the service is able to register
+            handles under. Example: '2286.9'
+        
+        @param suffix: A string unique in the namespace of the supplied prefix. 
+            If not supplied the server will generate one.
+        
+        @return: String representing the handle in the form of 'suffix/prefix'
+            Example: '2286.9/af4y7fkd'
+        '''
+        
+        req_params = {'target': target, 'prefix': prefix}
+        if suffix:
+            req_params['suffix'] = suffix
+        resp, cont = self.http.request(self.baseurl, 'POST', 
+                                       headers=dict([NO_CACHE, FORM_ENCODED]), 
+                                       body=urlencode(req_params))
         status = resp.get('status')
-        if status == '201': return resp.get('location')
-        if status == '409': raise HandleExists()
-        if status == '400': raise BadRequest()
+        if status == '201': 
+            return resp.get('location').lstrip(self.REGISTRY_BASE_URL)
+        if status == '409': 
+            raise HandleExists()
+        if status == '400': 
+            raise BadRequest()
         raise HandleServerException(cont)
     
     def read(self, handle):
-        resp, cont = self.http.request(self._resource_url(handle), 
-                                       'GET', headers=NO_CACHE)
+        '''Retrieve the target of the supplied handle.
+        
+        @param handle: in the form of 'prefix/suffix' 
+        
+        @return: URL the handle will resolve to.
+        '''
+        url = self._resource_url(handle)
+        resp, cont = self.http.request(url, 'GET', headers=dict([NO_CACHE]))
         status = resp.get('status')
-        if status == '204': return resp.get('location')
-        if status == '404': return None
-        if status == '400': raise BadRequest()
+        if status == '204': 
+            return resp.get('location')
+        if status == '404': 
+            return None
+        if status == '400': 
+            raise BadRequest()
         raise HandleServerException(cont)
     
     def update(self, handle, target, create=False):
-        resp, cont = self.http.request(self._request_url(handle, 
-                                                         target=target, 
-                                                         create=create), 
-                                       'PUT', headers=NO_CACHE)
+        '''Change the target URL that an existing handle resolves to.
+        
+        @param handle: The handle to change.
+        
+        @param target: Valid URL of the new target.
+        
+        @param create: If set to True the supplied handle will be created
+            with the specified target if it does not already exist.
+        
+        @return: The handle that was either updated or created.
+        '''
+        url = self._request_url(handle, target=target, create=create)
+        resp, cont = self.http.request(url, 'PUT', headers=dict([NO_CACHE]))
         status = resp.get('status')
-        if status == '201' or status == '204': return resp.get('location')
-        if status == '400': raise BadRequest()
-        if status == '404': raise HandleNotFound()
+        if status == '201' or status == '204': 
+            return resp.get('location').lstrip(self.REGISTRY_BASE_URL)
+        if status == '400': 
+            raise BadRequest()
+        if status == '404': 
+            raise HandleNotFound()
         raise HandleServerException(cont)
     
     def delete(self, handle):
+        '''Remove the handle completely from the registry.
+        
+        @param handle: The handle to remove.'''
         resp, cont = self.http.request(self._resource_url(handle),
-                                       'DELETE', headers=NO_CACHE)
+                                       'DELETE', headers=dict([NO_CACHE]))
         status = resp.get('status')
-        if status == '204': return True
-        if status == '400': raise BadRequest()
+        if status == '204': 
+            return True
+        if status == '400': 
+            raise BadRequest()
         raise HandleServerException(cont)
     
     def _resource_url(self, handle):
-        return self.baseurl + quote(handle)
+        if handle:
+            return self.baseurl + quote(handle)
+        return self.baseurl
     
-    def _request_url(self, handle, **kwargs):
-        return self._resource_url(handle) + '?' + urlencode(kwargs)
+    def _request_url(self, hdl, **kwargs):
+        return self._resource_url(hdl) + '?' + urlencode(kwargs)
 
 class HandleServerException(Exception):
     pass
--- handle/tests.py
+++ handle/tests.py
@@ -2,59 +2,69 @@ import unittest
 from client import Client, HandleExists, HandleNotFound, BadRequest
 
 class HandleClientTest(unittest.TestCase):
-    """This is extremely bad form for unit testing -- each test depends on the
-    successful run of a previous test. This is due to the fact we are testing 
-    both this client and the remote service. Open to better ways of doing this."""
+    """This is extremely bad form for unit testing. This is really an 
+    integration test because each scenario depends on the side effects of a 
+    successful run of a previous "unit". This is due to the fact we are testing 
+    this client against a remote service. Therefore, I am collapsing these 
+    connected actions into one test. I'm open to better ways of doing this."""
     
     HDL_SERVER_BASE = 'http://hdl.handle.net/'
     
     def setUp(self):
-        self.client = Client('http://localhost:8080/services/handle', 
-                             'handleAdmin', 'handletest')
-        self.createdHandle = '2286/py-handleclient:newone'
-        self.createdTarget = 'http://www.asu.edu'
-        self.updatedTarget = 'http://lib.asu.edu'
-        self.badTarget = 'asu.edu'
+        self.client = Client('http://handle_int.lib.asu.edu:8080/handleservice', 
+                             'handleAdmin', 'vj83j#W4')
+        self.prefix = '2286.9'
+        self.suffix = 'py-handleclient:newone'
+        self.handle = self.prefix + '/' + self.suffix
+        self.target = 'http://www.asu.edu'
+        self.new_target = 'http://lib.asu.edu'
+        self.bad_target = 'asu.edu'
     
-    def test_01_bad_request(self):
-        self.assertRaises(BadRequest, self.client.create,
-                          self.createdHandle, self.badTarget)
+    def test_bad_request(self):
+        self.assertRaises(BadRequest, self.client.create, self.bad_target,
+                          self.prefix, self.suffix)
     
-    def test_02_create(self):
-        hdl = self.client.create(self.createdHandle, self.createdTarget)
-        self.assertEqual('%s%s' % (HandleClientTest.HDL_SERVER_BASE, self.createdHandle), hdl)
-    
-    def test_03_create_handle_exists(self):
-        self.assertRaises(HandleExists,
-                          self.client.create, 
-                          self.createdHandle, 
-                          self.createdTarget)
-    
-    def test_04_read(self):
-        tgt = self.client.read(self.createdHandle)
-        self.assertEqual(tgt, self.createdTarget)
-    
-    def test_05_update(self):
-        hdl = self.client.update(self.createdHandle, self.updatedTarget)
-        self.assertEqual('%s%s' % (HandleClientTest.HDL_SERVER_BASE, self.createdHandle), hdl)
-    
-    def test_06_update_read(self):
-        self.assertEqual(self.client.read(self.createdHandle), self.updatedTarget)
-    
-    def test_07_delete(self):
-        self.assertTrue(self.client.delete(self.createdHandle))
-    
-    def test_08_read_not_found(self):
-        self.assertEqual(self.client.read(self.createdHandle), None)
-    
-    def test_09_update_not_found(self):
-        self.assertRaises(HandleNotFound, self.client.update, 
-                          self.createdHandle, self.createdTarget)
+    def test_supplied_handle(self):
+        hdl = self.client.create(self.target, self.prefix, self.suffix)
         
-    def test_10_update_with_create(self):
-        hdl = self.client.update(self.createdHandle, self.updatedTarget, create=True)
-        self.client.delete(self.createdHandle)
-        self.assertEqual('%s%s' % (HandleClientTest.HDL_SERVER_BASE, self.createdHandle), hdl)
+        # ensure the returned value looks like we expect
+        self.assertEqual('%s%s' % (self.HDL_SERVER_BASE, self.handle), hdl)
+        
+        # ensure we can not create one that already exists
+        self.assertRaises(HandleExists, self.client.create, self.target,
+                          self.prefix, self.suffix)
+        
+        # ensure expected handle target
+        self.assertEqual(self.client.read(self.handle), self.target)   
+        
+        # test that we can update the target
+        hdl = self.client.update(self.handle, self.new_target)
+        self.assertEqual('%s%s' % (self.HDL_SERVER_BASE, self.handle), hdl)
+        self.assertEqual(self.client.read(self.handle), self.new_target)
+        
+        # ensure we can delete
+        self.assertTrue(self.client.delete(self.handle))
+        
+        # make sure we can not find the handle we just deleted
+        self.assertEqual(self.client.read(self.handle), None)
+        
+        # make sure we can't update something that does not exist
+        self.assertRaises(HandleNotFound, self.client.update, 
+                          self.handle, self.target)
+        
+        # tell the update method to create it if not found
+        hdl = self.client.update(self.handle, self.target, create=True)
+        self.assertEqual('%s%s' % (self.HDL_SERVER_BASE, self.handle), hdl)
+
+        self.client.delete(self.handle)
+        
+    def test_generated_handle(self):
+        hdl_url = self.client.create(self.target, self.prefix)
+        hdl = hdl_url.lstrip(self.HDL_SERVER_BASE)
+        
+        # ensure expected handle target
+        self.assertEqual(self.client.read(hdl), self.target)
+        self.client.delete(hdl)
         
 if __name__ == "__main__":
     unittest.main()
