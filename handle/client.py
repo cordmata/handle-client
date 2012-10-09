f81c940dde2f997b1434d14bf38490ea78e039c0
Matt Cordial <matt.cordial@asu.edu>
2012-10-09 11:53:40 -0700

    Add tests.
    The other tests were BAAAAD. They required a running server. Now
    we are just mocking so to test you will require the Mock library.

--- handle/client.py
+++ handle/client.py
@@ -3,7 +3,7 @@ from urllib import quote
 
 import requests
 
-NO_CACHE = {'Cache-Control': 'no-cache'}
+NO_CACHE = {"Cache-Control": "no-cache"}
 
 
 class Client(object):
@@ -11,7 +11,7 @@ class Client(object):
     Administration Web Service."""
 
     def __init__(self, url, username, password):
-        self.baseurl = url.rstrip('/')
+        self.baseurl = url.rstrip("/")
         self.auth = (username, password)
 
     def create(self, handle, target):
@@ -34,12 +34,12 @@ class Client(object):
         """
         resp = requests.post(
             self.URL(handle),
-            data={'target': target},
+            data={"target": target},
             headers=NO_CACHE,
             auth=self.auth
         )
         if resp.status_code is 201:
-            return resp.headers.get('location')
+            return resp.headers.get("location")
         raise HandleError(resp.status_code)
 
     def read(self, handle):
@@ -51,29 +51,28 @@ class Client(object):
         @return: URL the handle will resolve to.
         """
         resp = requests.get(self.URL(handle), auth=self.auth, headers=NO_CACHE)
-
         if resp.status_code is 204:
-            return resp.headers.get('location')
+            return resp.headers.get("location")
         raise HandleError(resp.status_code)
 
     def update(self, handle, target):
-        '''Change the target URL that an existing handle resolves to.
+        """Change the target URL that an existing handle resolves to.
 
         @param handle: The handle to change.
 
         @param target: Valid URL of the new target.
 
         @return: The handle that was either updated or created.
-        '''
+        """
         resp = requests.put(
             self.URL(handle),
-            params={'target': target},
+            params={"target": target},
             auth=self.auth,
             headers=NO_CACHE
         )
         status = resp.status_code
         if status is 201 or status is 204:
-            return resp.headers.get('location')
+            return resp.headers.get("location")
         raise HandleError(status)
 
     def delete(self, handle):
@@ -94,19 +93,25 @@ class Client(object):
 class HandleError(Exception):
 
     def __init__(self, status):
-        if status is 400:
-            self.message = 'Bad request.'
-        if status is 401:
-            self.message = 'Authentication failed.'
-        if status is 403:
+        status = int(status)
+        if status == 400:
+            self.message = "Bad request."
+        elif status == 401:
+            self.message = "Authentication failed."
+        elif status == 403:
             self.message = "You don't have the right to do that."
-        if status is 404:
-            self.message = 'Handle not found.'
-        if status is 409:
-            self.message = 'Handle already exists.'
+        elif status == 404:
+            self.message = "Handle not found."
+        elif status == 409:
+            self.message = "Handle already exists."
+        else:
+            self.message = "Handle Server error."
 
     def __unicode__(self):
         return unicode(self.message)
 
     def __str__(self):
         return self.message
+
+    def __repr__(self):
+        return self.__str__()
