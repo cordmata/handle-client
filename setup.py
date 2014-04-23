d2dc41d9823622d55cf27d0754c918750f4cba49
Matt Cordial <matt.cordial@asu.edu>
2014-04-23 13:54:51 -0700

    Bump version for tag.

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "py-handleclient",
-    version = "0.5",
-    packages = find_packages(),
-    install_requires = [
-        'requests >= 0.14'
+    name="py-handleclient",
+    version="0.6",
+    packages=find_packages(),
+    install_requires=[
+        'requests >= 2.0'
     ],
-    author = "Matt Cordial",
-    author_email = "matt.cordial@asu.edu",
-    description = "A client which communicates with the Handle Administration Web Service.",
-    keywords = "cnri handle server web-service",
+    author="Matt Cordial",
+    author_email="matt.cordial@asu.edu",
+    description=(
+        "A client which communicates with the Handle Administration "
+        "Web Service."
+    ),
+    keywords="cnri handle server web-service",
 )
